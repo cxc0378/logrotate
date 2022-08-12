7d205fe747034f3036f95d2ed388a62920de0121
cxc0378 <cxc-0378@163.com>
2022-08-12 23:04:10 +0800

    Update logRotate.php

--- logRotate.php
+++ logRotate.php
@@ -136,11 +136,4 @@ class LogRotate
 
 }
 
-//usage:
-
-$logRt = LogRotate::getInstance();
-$logRt->init(5, 2, './logs', 'mylog', 10);
-$logRt->debug('This is a debug message');
-$logRt->info('This is a info message');
-$logRt->error('This is a error message');
 
